209a5221617519b4b1104d4e657dc7e4ec4f3bfa
electhuang <electhuang@gmail.com>
2016-11-15 23:40:26 +0800

    修改状态栏与应用主题颜色一致，在内容显示区域添加了对应的Fragment

--- app/src/main/java/com/electhuang/here/MainActivity.java
+++ app/src/main/java/com/electhuang/here/MainActivity.java
@@ -1,23 +1,85 @@
 package com.electhuang.here;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends BaseActivity
 		implements NavigationView.OnNavigationItemSelectedListener {
+
+	private Toolbar toolbar;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+		initBar();
+		initBottomNavigationView();
+	}
+
+	/**
+	 * 初始化底部导航栏
+	 */
+	private void initBottomNavigationView() {
+		BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id
+				.bottomNavigationView);
+		bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView
+				.OnNavigationItemSelectedListener() {
+
+			@Override
+			public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+				switch (item.getItemId()) {
+					case R.id.registration:
+						Fragment registrationFragment = new RegistrationFragment();
+						replaceContent(registrationFragment);
+						toolbar.setTitle("这里签到·Here");
+						break;
+					case R.id.add:
+						Fragment addFragment = new addFragment();
+						replaceContent(addFragment);
+						toolbar.setTitle("添加签到");
+						break;
+					case R.id.setting:
+						Fragment settingFragmen = new SettingFragmen();
+						replaceContent(settingFragmen);
+						toolbar.setTitle("设置");
+						break;
+				}
+				return true;
+			}
+		});
+	}
+
+	/**
+	 * 替换内容界面的Fragment
+	 *
+	 * @param fragment 需要替换成的Fragment
+	 */
+	private void replaceContent(Fragment fragment) {
+		FragmentManager fragmentManager = getSupportFragmentManager();
+		FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+		fragmentTransaction.replace(R.id.contentLayout, fragment).commit();
+		invalidateOptionsMenu();
+	}
+
+	/**
+	 * 初始化ToolBar和NavigationView
+	 */
+	private void initBar() {
+		//设置状态栏颜色与应用主题颜色一致
+		setStatusBarColor(this, 0xFF0288D1);
+		toolbar = (Toolbar) findViewById(R.id.toolbar);
+		toolbar.setTitle("这里签到·Here");
 		setSupportActionBar(toolbar);
 
 		DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -71,12 +133,18 @@ public class MainActivity extends AppCompatActivity
 		int id = item.getItemId();
 
 		if (id == R.id.registration) {
-			// Handle the camera action
+			Fragment registrationFragment = new RegistrationFragment();
+			replaceContent(registrationFragment);
+			toolbar.setTitle("这里签到·Here");
 		} else if (id == R.id.add) {
-
+			Fragment addFragment = new addFragment();
+			replaceContent(addFragment);
+			toolbar.setTitle("添加签到");
 		} else if (id == R.id.setting) {
-
-		}  else if (id == R.id.nav_share) {
+			Fragment settingFragmen = new SettingFragmen();
+			replaceContent(settingFragmen);
+			toolbar.setTitle("设置");
+		} else if (id == R.id.nav_share) {
 
 		} else if (id == R.id.nav_send) {
 
--- app/src/main/java/com/electhuang/here/addFragment.java
+++ app/src/main/java/com/electhuang/here/addFragment.java
@@ -6,14 +6,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class RegistrationFragment extends Fragment {
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class addFragment extends Fragment {
 
+	public addFragment() {
+		// Required empty public constructor
+	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 	                         Bundle savedInstanceState) {
 		// Inflate the layout for this fragment
-		View view = inflater.inflate(R.layout.fragment_registration, container, false);
+		View view = inflater.inflate(R.layout.fragment_add, container, false);
 		return view;
 	}
+
 }
